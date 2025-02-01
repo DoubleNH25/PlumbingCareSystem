1f93afccba4ca57a9386e38916033b54a49cd08e
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-01 21:54:41 +0700

    AutoMapper And ViewModel Setup

---
+++ PlumpingCareSystem.Entity/WebApplication/ViewModels/Team/TeamListVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlumpingCareSystem.Entity.WebApplication.ViewModels.Team
+{
+	internal class TeamListVM
+	{
+	}
+}
---
+++ PlumpingCareSystem.Entity/WebApplication/ViewModels/Testimonal/TestimonalUpdateVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlumpingCareSystem.Entity.WebApplication.ViewModels.Testimonal
+{
+	internal class TestimonalUpdateVM
+	{
+	}
+}
