9a1c50f3f08b3cccd640ec9ce5c50bd8174410c7
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-01 14:54:51 +0700

    Add Migration And Update Database

--- PlumpingCareSystem/Controllers/HomeController.cs
+++ PlumpingCareSystem/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using PlumpingCareSystem.Models;
+
 
 namespace PlumpingCareSystem.Controllers
 {
@@ -23,10 +23,10 @@ namespace PlumpingCareSystem.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+/*        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        }*/
     }
 }
