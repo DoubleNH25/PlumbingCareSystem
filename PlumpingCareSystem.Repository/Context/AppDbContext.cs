a1af74a63f4674e73a7f6cdc3849040c3e7a29b4
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-01 10:20:25 +0700

    Setup DbContext

---
+++ PlumpingCareSystem.Repository/Context/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using PlumpingCareSystem.Entity.WebApplication.Entities;
+using System.Reflection;
+
+namespace PlumpingCareSystem.Repository.Context
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		protected AppDbContext()
+		{
+		}
+
+		public DbSet<About> Abouts { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Contact> Contacts { get; set; }
+		public DbSet<HomePage> HomePages { get; set; }
+		public DbSet<Portfolio> Portfolios { get; set; }
+		public DbSet<Service> Services { get; set; }
+		public DbSet<SocialMedia> SocialMedias { get; set; }
+		public DbSet<Team> Teams { get; set; }
+		public DbSet<Testimonal> Testimonals { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
