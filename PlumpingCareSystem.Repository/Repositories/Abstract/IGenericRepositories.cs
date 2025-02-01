cf46f6e837e855bc37c21ed5edbc20d9f9a3b87f
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-01 15:13:06 +0700

    Create Generic Repository With CRUD

---
+++ PlumpingCareSystem.Repository/Repositories/Abstract/IGenericRepositories.cs
@@ -0,0 +1,17 @@
+﻿using PlumpingCareSystem.Core.BaseEntity;
+using System.Linq.Expressions;
+
+namespace PlumpingCareSystem.Repository.Repositories.Abstract
+{
+	public interface IGenericRepositories<T> where T : class, IBaseEntity, new()
+	{
+		Task AddEntityAsync(T entity);
+		void UpdatetEntity(T entity);
+		void DeletetEntity(T entity);
+		IQueryable<T> GetAlltEntityList();
+		IQueryable<T> Where(Expression<Func<T, bool>> predicate);
+		Task<T> GetEntityByIdAsync(int id);
+		Task<int> GetAllCount();
+
+	}
+}
---
+++ PlumpingCareSystem.Repository/Repositories/Concrete/GenericRepositories.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlumpingCareSystem.Core.BaseEntity;
+using PlumpingCareSystem.Repository.Context;
+using PlumpingCareSystem.Repository.Repositories.Abstract;
+using System.Linq.Expressions;
+
+namespace PlumpingCareSystem.Repository.Repositories.Concrete
+{
+	public class GenericRepositories<T> : IGenericRepositories<T> where T : class, IBaseEntity, new()
+	{
+		private readonly AppDbContext _context;
+		private readonly DbSet<T> _dbSet;
+
+		public GenericRepositories(AppDbContext context)
+		{
+			_context = context;
+			_dbSet = _context.Set<T>();
+		}
+
+
+
+		public async Task AddEntityAsync(T entity)
+		{
+			await _dbSet.AddAsync(entity);
+		}
+
+		public void UpdatetEntity(T entity)
+		{
+			_dbSet.Update(entity);
+		}
+
+		public void DeletetEntity(T entity)
+		{
+			_dbSet.Remove(entity);
+		}
+
+		public IQueryable<T> GetAlltEntityList()
+		{
+			return _dbSet.AsNoTracking().AsQueryable();
+		}
+
+		public IQueryable<T> Where(Expression<Func<T, bool>> predicate)
+		{
+			return _dbSet.Where(predicate);
+		}
+
+		public async Task<T> GetEntityByIdAsync(int id)
+		{
+			return await _dbSet.FindAsync(id);
+		}
+
+		public async Task<int> GetAllCount()
+		{
+			return await _dbSet.CountAsync();
+		}
+	}
+}
