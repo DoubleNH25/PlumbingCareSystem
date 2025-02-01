ad261ddf9efe81dab961215033606f91916196e7
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-01 10:57:02 +0700

    Setup RepositoryLayerExtension

---
+++ PlumpingCareSystem.Repository/Extensions/RepositoryLayerExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PlumpingCareSystem.Repository.Context;
+
+
+namespace PlumpingCareSystem.Repository.Extensions
+{
+	public static class RepositoryLayerExtensions
+	{
+		public static IServiceCollection LoadRepositoryLayerExtensions(this IServiceCollection services, IConfiguration config)
+		{
+			services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(config.GetConnectionString("SqlConnection")));
+
+			return services;
+
+		}
+	}
+}
--- PlumpingCareSystem/Program.cs
+++ PlumpingCareSystem/Program.cs
@@ -1,7 +1,11 @@
+using PlumpingCareSystem.Repository.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.LoadRepositoryLayerExtensions(builder.Configuration);
 
 var app = builder.Build();
 
