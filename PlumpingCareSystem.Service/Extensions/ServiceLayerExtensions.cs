57e697e803af17fedbef7ad3cc10e31e7f1abb31
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-04 06:27:47 +0700

    Add New Service For DI

--- PlumpingCareSystem.Service/Extensions/ServiceLayerExtensions.cs
+++ PlumpingCareSystem.Service/Extensions/ServiceLayerExtensions.cs
@@ -8,6 +8,19 @@ namespace PlumpingCareSystem.Service.Extensions
 		public static IServiceCollection LoadServiceLayerExtensions(this IServiceCollection services)
 		{
 			services.AddAutoMapper(Assembly.GetExecutingAssembly());
+			
+			var types = Assembly.GetExecutingAssembly().GetTypes()
+				.Where(x => x.IsClass && !x.IsAbstract && x.Name.EndsWith("Service"));
+
+			foreach (var serviceType in types)
+			{
+				var iServiceType = serviceType.GetInterfaces().FirstOrDefault(x => x.Name == $"I{serviceType.Name}");
+				if (iServiceType != null)
+				{
+					services.AddScoped(iServiceType, serviceType);
+				}
+			}
+
 			return services;
 		}
 	}
