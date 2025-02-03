be4fdc8a5bd9e1d271a997205933081eee55518f
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-04 06:43:15 +0700

    About Controller CRUD Operations

---
+++ PlumpingCareSystem/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.AboutVM;
+using PlumpingCareSystem.Service.ServiceHolding.Abstract;
+
+namespace PlumpingCareSystem.Areas.Admin.Controllers
+{
+	public class AboutController : Controller
+	{
+		private readonly IAboutService _aboutService;
+
+		public AboutController(IAboutService aboutService)
+		{
+			_aboutService = aboutService;
+		}
+
+		public async Task<IActionResult> GetAboutList()
+		{
+			var aboutList = await _aboutService.GetAllListAsync();
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult AddAbout()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddAbout(AboutAddVM request)
+		{
+			await _aboutService.AddAboutAsync(request);
+			return RedirectToAction("GetAboutList","About", new {Area=("Admin")});
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> UpdateAbout(int id)
+		{
+			var about = await _aboutService.GetAboutById(id);
+			return View(about);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> UpdateAbout(AboutUpdateVM request)
+		{
+			await _aboutService.UpdateAboutAsync(request);
+			return RedirectToAction("GetAboutList", "About", new { Area = ("Admin") });
+		}
+
+		public async Task<IActionResult> DeleteAbout(int id)
+		{
+			await _aboutService.DeleteAboutAsync(id);
+			return RedirectToAction("GetAboutList", "About", new { Area = ("Admin") });
+		}
+	}
+}
