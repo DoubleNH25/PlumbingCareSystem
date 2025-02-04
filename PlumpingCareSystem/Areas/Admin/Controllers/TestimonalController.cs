408448a0d4ab3ef93d3efc3fca626f48f2ce8073
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-04 15:54:10 +0700

    Testimonal Controller And View Setup

---
+++ PlumpingCareSystem/Areas/Admin/Controllers/TestimonalController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Testimonal;
+using PlumpingCareSystem.Service.ServiceHolding.Abstract;
+
+namespace PlumpingCareSystem.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class TestimonalController : Controller
+	{
+		private readonly ITestimonalService _testimonalService;
+		public TestimonalController(ITestimonalService testimonalService)
+		{
+			_testimonalService = testimonalService;
+		}
+		public async Task<IActionResult> GetTestimonalList()
+		{
+			var testimonalList = await _testimonalService.GetAllListAsync();
+			return View(testimonalList);
+		}
+		[HttpGet]
+		public IActionResult AddTestimonal()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> AddTestimonal(TestimonalAddVM request)
+		{
+			await _testimonalService.AddTestimonalAsync(request);
+			return RedirectToAction("GetTestimonalList", "Testimonal", new { Area = ("Admin") });
+		}
+		[HttpGet]
+		public async Task<IActionResult> UpdateTestimonal(int id)
+		{
+			var testimonal = await _testimonalService.GetTestimonalById(id);
+			return View(testimonal);
+		}
+		[HttpPost]
+		public async Task<IActionResult> UpdateTestimonal(TestimonalUpdateVM request)
+		{
+			await _testimonalService.UpdateTestimonalAsync(request);
+			return RedirectToAction("GetTestimonalList", "Testimonal", new { Area = ("Admin") });
+		}
+		public async Task<IActionResult> DeleteTestimonal(int id)
+		{
+			await _testimonalService.DeleteTestimonalAsync(id);
+			return RedirectToAction("GetTestimonalList", "Testimonal", new { Area = ("Admin") });
+		}
+	}
+}
