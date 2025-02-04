136b0ec85a88517408b0796f2188eb1276ce7175
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-04 15:26:30 +0700

    HomePage Controller And View Setup

---
+++ PlumpingCareSystem/Areas/Admin/Controllers/HomePageController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.HomePage;
+using PlumpingCareSystem.Service.ServiceHolding.Abstract;
+
+namespace PlumpingCareSystem.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class HomePageController : Controller
+	{
+		private readonly IHomePageService _homePageService;
+		public HomePageController(IHomePageService homePageService)
+		{
+			_homePageService = homePageService;
+		}
+		public async Task<IActionResult> GetHomePageList()
+		{
+			var homePageList = await _homePageService.GetAllListAsync();
+			return View(homePageList);
+		}
+		[HttpGet]
+		public IActionResult AddHomePage()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> AddHomePage(HomePageAddVM request)
+		{
+			await _homePageService.AddHomePageAsync(request);
+			return RedirectToAction("GetHomePageList", "HomePage", new { Area = ("Admin") });
+		}
+		[HttpGet]
+		public async Task<IActionResult> UpdateHomePage(int id)
+		{
+			var homePage = await _homePageService.GetHomePageById(id);
+			return View(homePage);
+		}
+		[HttpPost]
+		public async Task<IActionResult> UpdateHomePage(HomePageUpdateVM request)
+		{
+			await _homePageService.UpdateHomePageAsync(request);
+			return RedirectToAction("GetHomePageList", "HomePage", new { Area = ("Admin") });
+		}
+		public async Task<IActionResult> DeleteHomePage(int id)
+		{
+			await _homePageService.DeleteHomePageAsync(id);
+			return RedirectToAction("GetHomePageList", "HomePage", new { Area = ("Admin") });
+		}
+	}
+}
