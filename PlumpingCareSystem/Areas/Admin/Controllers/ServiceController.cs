8dcad3f28dd39387f142f80e1c80b79a669e09b8
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-04 15:45:16 +0700

    Service Controller And View Setup

---
+++ PlumpingCareSystem/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Service;
+using PlumpingCareSystem.Service.ServiceHolding.Abstract;
+
+namespace PlumpingCareSystem.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class ServiceController : Controller
+	{
+		private readonly IServiceService _serviceService;
+		public ServiceController(IServiceService serviceService)
+		{
+			_serviceService = serviceService;
+		}
+
+		public async Task<IActionResult> GetServiceList()
+		{
+			var serviceList = await _serviceService.GetAllListAsync();
+			return View(serviceList);
+		}
+		[HttpGet]
+		public IActionResult AddService()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> AddService(ServiceAddVM request)
+		{
+			await _serviceService.AddServiceAsync(request);
+			return RedirectToAction("GetServiceList", "Service", new { Area = ("Admin") });
+		}
+		[HttpGet]
+		public async Task<IActionResult> UpdateService(int id)
+		{
+			var service = await _serviceService.GetServiceById(id);
+			return View(service);
+		}
+		[HttpPost]
+		public async Task<IActionResult> UpdateService(ServiceUpdateVM request)
+		{
+			await _serviceService.UpdateServiceAsync(request);
+			return RedirectToAction("GetServiceList", "Service", new { Area = ("Admin") });
+		}
+		public async Task<IActionResult> DeleteService(int id)
+		{
+			await _serviceService.DeleteServiceAsync(id);
+			return RedirectToAction("GetServiceList", "Service", new { Area = ("Admin") });
+		}
+	}
+}
