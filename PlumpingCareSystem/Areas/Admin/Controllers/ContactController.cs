852d1c6f61b1ce682f2f4725d3f6c6c9f87ecebb
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-04 14:45:44 +0700

    Completed Category Controller CRUD And View Setup

---
+++ PlumpingCareSystem/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Contact;
+using PlumpingCareSystem.Service.ServiceHolding.Abstract;
+
+namespace PlumpingCareSystem.Areas.Admin.Controllers
+{
+		[Area("Admin")]
+		public class ContactController : Controller
+		{
+			private readonly IContactService _contactService;
+			public ContactController(IContactService contactService)
+			{
+				_contactService = contactService;
+			}
+			public async Task<IActionResult> GetContactList()
+			{
+				var contactList = await _contactService.GetAllListAsync();
+				return View(contactList);
+			}
+			[HttpGet]
+			public IActionResult AddContact()
+			{
+				return View();
+			}
+			[HttpPost]
+			public async Task<IActionResult> AddContact(ContactAddVM request)
+			{
+				await _contactService.AddContactAsync(request);
+				return RedirectToAction("GetContactList", "Contact", new { Area = ("Admin") });
+			}
+			[HttpGet]
+			public async Task<IActionResult> UpdateContact(int id)
+			{
+				var contact = await _contactService.GetContactById(id);
+				return View(contact);
+			}
+			[HttpPost]
+			public async Task<IActionResult> UpdateContact(ContactUpdateVM request)
+			{
+				await _contactService.UpdateContactAsync(request);
+				return RedirectToAction("GetContactList", "Contact", new { Area = ("Admin") });
+			}
+			public async Task<IActionResult> DeleteContact(int id)
+			{
+				await _contactService.DeleteContactAsync(id);
+				return RedirectToAction("GetContactList", "Contact", new { Area = ("Admin") });
+			}
+		}
+	}
