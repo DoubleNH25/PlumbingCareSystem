d4b32a7646880264763e1d87c0159efe74ecc469
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-05 10:36:07 +0700

    Team Validation Setup

--- PlumpingCareSystem.Service/FluentValidation/WebApplication/TeamValidation/TeamAddValidation.cs
+++ PlumpingCareSystem.Service/FluentValidation/WebApplication/TeamValidation/TeamAddValidation.cs
@@ -1,7 +1,27 @@
 ﻿
+using FluentValidation;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Team;
+using PlumpingCareSystem.Service.Messages.WebApplication;
+
 namespace PlumpingCareSystem.Service.FluentValidation.WebApplication.TeamValidation
 {
-	internal class TeamAddValidation
+	public class TeamAddValidation : AbstractValidator<TeamAddVM>
 	{
+		public TeamAddValidation()
+		{
+			RuleFor(x => x.FullName)
+			  .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("FullName"))
+			  .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("FullName"))
+			  .MaximumLength(100).WithMessage(ValidationMessages.MaximumCharachterAllowence("FullName", 100));
+			RuleFor(x => x.Title)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .MaximumLength(100).WithMessage(ValidationMessages.MaximumCharachterAllowence("Title", 100));
+
+
+			RuleFor(x => x.Photo)
+				.NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Photo"))
+				.NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Photo"));
+		}
 	}
 }
--- PlumpingCareSystem.Service/FluentValidation/WebApplication/TeamValidation/TeamUpdateValidation.cs
+++ PlumpingCareSystem.Service/FluentValidation/WebApplication/TeamValidation/TeamUpdateValidation.cs
@@ -1,8 +1,24 @@
 ﻿
 
+using FluentValidation;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Team;
+using PlumpingCareSystem.Service.Messages.WebApplication;
+
 namespace PlumpingCareSystem.Service.FluentValidation.WebApplication.TeamValidation
 {
-	internal class TeamUpdateValidation
+	public class TeamUpdateValidation : AbstractValidator<TeamUpdateVM>
 	{
+		public TeamUpdateValidation()
+		{
+			RuleFor(x => x.FullName)
+			  .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("FullName"))
+			  .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("FullName"))
+			  .MaximumLength(100).WithMessage(ValidationMessages.MaximumCharachterAllowence("FullName", 100));
+			RuleFor(x => x.Title)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .MaximumLength(100).WithMessage(ValidationMessages.MaximumCharachterAllowence("Title", 100));
+
+		}
 	}
 }
