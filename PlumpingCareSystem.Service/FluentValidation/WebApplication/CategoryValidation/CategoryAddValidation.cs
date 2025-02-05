b4243e1d275609534a73d1fe827b341470611c76
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-05 10:23:31 +0700

    Category Validation Setup

--- PlumpingCareSystem.Service/FluentValidation/WebApplication/CategoryValidation/CategoryAddValidation.cs
+++ PlumpingCareSystem.Service/FluentValidation/WebApplication/CategoryValidation/CategoryAddValidation.cs
@@ -1,8 +1,19 @@
 ﻿
 
+using FluentValidation;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Category;
+using PlumpingCareSystem.Service.Messages.WebApplication;
+
 namespace PlumpingCareSystem.Service.FluentValidation.WebApplication.CategoryValidation
 {
-	internal class CategoryAddValidation
+	public class CategoryAddValidation : AbstractValidator<CategoryAddVM>
 	{
+		public CategoryAddValidation()
+		{
+			RuleFor(x => x.Name)
+				.NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Name"))
+				.NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Name"))
+				.MaximumLength(50).WithMessage(ValidationMessages.MaximumCharachterAllowence("Name", 50));
+		}
 	}
 }
--- PlumpingCareSystem.Service/FluentValidation/WebApplication/CategoryValidation/CategoryUpdateValidation.cs
+++ PlumpingCareSystem.Service/FluentValidation/WebApplication/CategoryValidation/CategoryUpdateValidation.cs
@@ -1,8 +1,19 @@
 ﻿
 
+using FluentValidation;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Category;
+using PlumpingCareSystem.Service.Messages.WebApplication;
+
 namespace PlumpingCareSystem.Service.FluentValidation.WebApplication.CategoryValidation
 {
-	internal class CategoryUpdateValidation
+	public class CategoryUpdateValidation : AbstractValidator<CategoryUpdateVM>
 	{
+		public CategoryUpdateValidation()
+		{
+			RuleFor(x => x.Name)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Name"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Name"))
+			   .MaximumLength(50).WithMessage(ValidationMessages.MaximumCharachterAllowence("Name", 50));
+		}
 	}
 }
