6371dbbcfb4362c9f1e7dec0d478347c504ce15c
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-05 10:24:49 +0700

    Contact Validation Setup

--- PlumpingCareSystem.Service/FluentValidation/WebApplication/ContactValidation/ContactAddValidation.cs
+++ PlumpingCareSystem.Service/FluentValidation/WebApplication/ContactValidation/ContactAddValidation.cs
@@ -1,8 +1,30 @@
 ﻿
 
+using FluentValidation;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Contact;
+using PlumpingCareSystem.Service.Messages.WebApplication;
+
 namespace PlumpingCareSystem.Service.FluentValidation.WebApplication.ContactValidation
 {
-	internal class ContactAddValidation
+	public class ContactAddValidation : AbstractValidator<ContactAddVM>
 	{
+		public ContactAddValidation()
+		{
+			RuleFor(x => x.Location)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Location"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Location"))
+			   .MaximumLength(200).WithMessage(ValidationMessages.MaximumCharachterAllowence("Location", 200));
+			RuleFor(x => x.Email)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Email"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Email"))
+			   .MaximumLength(100).WithMessage(ValidationMessages.MaximumCharachterAllowence("Location", 100));
+			RuleFor(x => x.Call)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Call"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Call"))
+			   .MaximumLength(17).WithMessage(ValidationMessages.MaximumCharachterAllowence("Location", 17));
+			RuleFor(x => x.Map)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Map"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Map"));
+		}
 	}
 }
