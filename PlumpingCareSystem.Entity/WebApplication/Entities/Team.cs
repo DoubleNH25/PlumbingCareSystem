233f7ba8ba76cb3ab56842daeb31f85ba4967056
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-05 14:43:36 +0700

    Completed All View Validation

--- PlumpingCareSystem.Entity/WebApplication/Entities/Team.cs
+++ PlumpingCareSystem.Entity/WebApplication/Entities/Team.cs
@@ -1,9 +1,4 @@
 ﻿using PlumpingCareSystem.Core.BaseEntity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PlumpingCareSystem.Entity.WebApplication.Entities
 {
--- PlumpingCareSystem.Entity/WebApplication/Entities/Testimonal.cs
+++ PlumpingCareSystem.Entity/WebApplication/Entities/Testimonal.cs
@@ -1,9 +1,5 @@
 ﻿using PlumpingCareSystem.Core.BaseEntity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace PlumpingCareSystem.Entity.WebApplication.Entities
 {
