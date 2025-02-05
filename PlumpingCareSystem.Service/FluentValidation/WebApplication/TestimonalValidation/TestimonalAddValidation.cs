44d83a84bce209b1bd8b154277e265c186e0b315
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-05 10:37:22 +0700

    Testimonal Validation Setup

--- PlumpingCareSystem.Service/FluentValidation/WebApplication/TestimonalValidation/TestimonalAddValidation.cs
+++ PlumpingCareSystem.Service/FluentValidation/WebApplication/TestimonalValidation/TestimonalAddValidation.cs
@@ -1,8 +1,31 @@
 ﻿
 
+using FluentValidation;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Testimonal;
+using PlumpingCareSystem.Service.Messages.WebApplication;
+
 namespace PlumpingCareSystem.Service.FluentValidation.WebApplication.TestimonalValidation
 {
-	internal class TestimonalAddValidation
+	public class TestimonalAddValidaton : AbstractValidator<TestimonalAddVM>
 	{
+		public TestimonalAddValidaton()
+		{
+			RuleFor(x => x.FullName)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("FullName"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("FullName"))
+			   .MaximumLength(100).WithMessage(ValidationMessages.MaximumCharachterAllowence("FullName", 100));
+			RuleFor(x => x.Title)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .MaximumLength(100).WithMessage(ValidationMessages.MaximumCharachterAllowence("Title", 100));
+			RuleFor(x => x.Comment)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Comment"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Comment"))
+			   .MaximumLength(2000).WithMessage(ValidationMessages.MaximumCharachterAllowence("Comment", 2000)); ;
+
+			RuleFor(x => x.Photo)
+			  .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Photo"))
+			  .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Photo"));
+		}
 	}
 }
--- PlumpingCareSystem.Service/FluentValidation/WebApplication/TestimonalValidation/TestimonalUpdateValidation.cs
+++ PlumpingCareSystem.Service/FluentValidation/WebApplication/TestimonalValidation/TestimonalUpdateValidation.cs
@@ -1,8 +1,28 @@
 ﻿
 
+using FluentValidation;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Testimonal;
+using PlumpingCareSystem.Service.Messages.WebApplication;
+
 namespace PlumpingCareSystem.Service.FluentValidation.WebApplication.TestimonalValidation
 {
-	internal class TestimonalUpdateValidation
+	public class TestimonalUpdateValidation : AbstractValidator<TestimonalUpdateVM>
 	{
+		public TestimonalUpdateValidation()
+		{
+			RuleFor(x => x.FullName)
+				.NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("FullName"))
+				.NotNull().WithMessage(ValidationMessages.NullEmptyMessage("FullName"))
+				.MaximumLength(100).WithMessage(ValidationMessages.MaximumCharachterAllowence("FullName", 100));
+			RuleFor(x => x.Title)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .MaximumLength(100).WithMessage(ValidationMessages.MaximumCharachterAllowence("Title", 100));
+			RuleFor(x => x.Comment)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Comment"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Comment"))
+			   .MaximumLength(2000).WithMessage(ValidationMessages.MaximumCharachterAllowence("Comment", 2000)); ;
+
+		}
 	}
 }
