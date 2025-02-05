94b14b01669ff36095fd12d01a8b0006fe2802ee
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-05 10:07:34 +0700

    Create ValidationMessages & Use It For About Validation (Add & Update)

---
+++ PlumpingCareSystem.Service/Messages/WebApplication/ValidationMessages.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace PlumpingCareSystem.Service.Messages.WebApplication
+{
+	public static class ValidationMessages
+	{
+		public static string NullEmptyMessage(string propName)
+		{
+			return $"{propName} must have a value!";
+		}
+
+		public static string MaximumCharachterAllowence(string propName, int resctriction)
+		{
+			return $"{propName} can have maximum {resctriction}!";
+		}
+
+		public static string GreaterThanMessage(string propName, int restriction)
+		{
+			return $"{propName} must be greater than {restriction}";
+		}
+
+		public static string LessThanMessage(string propName, int restriction)
+		{
+			return $"{propName} must be less than {restriction}";
+		}
+	}
+}
