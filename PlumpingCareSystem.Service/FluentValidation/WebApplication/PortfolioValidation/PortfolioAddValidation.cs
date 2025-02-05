281e3a3f4eb55d73223bb8bac998fc0879fed889
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-05 10:34:06 +0700

    Portfolio Validation Setup

--- PlumpingCareSystem.Service/FluentValidation/WebApplication/PortfolioValidation/PortfolioAddValidation.cs
+++ PlumpingCareSystem.Service/FluentValidation/WebApplication/PortfolioValidation/PortfolioAddValidation.cs
@@ -1,8 +1,23 @@
 ﻿
 
+using FluentValidation;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Portfolio;
+using PlumpingCareSystem.Service.Messages.WebApplication;
+
 namespace PlumpingCareSystem.Service.FluentValidation.WebApplication.PortfolioValidation
 {
-	internal class PortfolioAddValidation
+	public class PortfolioAddValidation : AbstractValidator<PortfolioAddVM>
 	{
+		public PortfolioAddValidation()
+		{
+			RuleFor(x => x.Title)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .MaximumLength(200).WithMessage(ValidationMessages.MaximumCharachterAllowence("Title", 200));
+
+			RuleFor(x => x.Photo)
+				.NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Photo"))
+				.NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Photo"));
+		}
 	}
 }
--- PlumpingCareSystem.Service/FluentValidation/WebApplication/PortfolioValidation/PortfolioUpdateValidation.cs
+++ PlumpingCareSystem.Service/FluentValidation/WebApplication/PortfolioValidation/PortfolioUpdateValidation.cs
@@ -1,8 +1,21 @@
 ﻿
 
+using FluentValidation;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Portfolio;
+using PlumpingCareSystem.Service.Messages.WebApplication;
+
 namespace PlumpingCareSystem.Service.FluentValidation.WebApplication.PortfolioValidation
 {
-	internal class PortfolioUpdateValidation
+	public class PortfolioUpdateValidation : AbstractValidator<PortfolioUpdateVM>
 	{
+		public PortfolioUpdateValidation()
+		{
+
+			RuleFor(x => x.Title)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Title"))
+			   .MaximumLength(200).WithMessage(ValidationMessages.MaximumCharachterAllowence("Title", 200));
+
+		}
 	}
 }
