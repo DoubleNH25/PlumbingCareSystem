7c36fa16a50507b8f5b75970ba9169ec9bfd19e5
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-05 10:35:08 +0700

    Service Validation Setup

--- PlumpingCareSystem.Service/FluentValidation/WebApplication/ServiceValidation/ServiceAddValidation.cs
+++ PlumpingCareSystem.Service/FluentValidation/WebApplication/ServiceValidation/ServiceAddValidation.cs
@@ -1,8 +1,27 @@
 ﻿
 
+using FluentValidation;
+using PlumpingCareSystem.Entity.WebApplication.ViewModels.Service;
+using PlumpingCareSystem.Service.Messages.WebApplication;
+
 namespace PlumpingCareSystem.Service.FluentValidation.WebApplication.ServiceValidation
 {
-	internal class ServiceAddValidation
+	public class ServiceAddValidation : AbstractValidator<ServiceAddVM>
 	{
+		public ServiceAddValidation()
+		{
+			RuleFor(x => x.Name)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Name"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Name"))
+			   .MaximumLength(200).WithMessage(ValidationMessages.MaximumCharachterAllowence("Name", 200));
+			RuleFor(x => x.Description)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Description"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Description"))
+			   .MaximumLength(2000).WithMessage(ValidationMessages.MaximumCharachterAllowence("Description", 2000));
+			RuleFor(x => x.Icon)
+			   .NotEmpty().WithMessage(ValidationMessages.NullEmptyMessage("Icon"))
+			   .NotNull().WithMessage(ValidationMessages.NullEmptyMessage("Icon"))
+			   .MaximumLength(100).WithMessage(ValidationMessages.MaximumCharachterAllowence("Icon", 100));
+		}
 	}
 }
