890076c0120043927b221b161f28862881d80a88
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-16 13:37:38 +0700

    Picture Implementation For Team & Testimonals

--- PlumpingCareSystem.Service/ServiceHolding/WebApplication/Concrete/TeamService.cs
+++ PlumpingCareSystem.Service/ServiceHolding/WebApplication/Concrete/TeamService.cs
@@ -3,10 +3,12 @@
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
+using PlumpingCareSystem.Core.Enumerators;
 using PlumpingCareSystem.Entity.WebApplication.Entities;
 using PlumpingCareSystem.Entity.WebApplication.ViewModels.Team;
 using PlumpingCareSystem.Repository.Repositories.Abstract;
 using PlumpingCareSystem.Repository.UnitOfWorks.Abstract;
+using PlumpingCareSystem.Service.Helpers.Generic.Image;
 using PlumpingCareSystem.Service.ServiceHolding.WebApplication.Abstract;
 
 namespace PlumpingCareSystem.Service.ServiceHolding.WebApplication.Concrete
@@ -16,11 +18,13 @@ namespace PlumpingCareSystem.Service.ServiceHolding.WebApplication.Concrete
 		private readonly IUnitOfWork _unitOfWork;
 		private readonly IMapper _mapper;
 		private readonly IGenericRepositories<Team> _repository;
-		public TeamService(IUnitOfWork unitOfWork, IMapper mapper)
+		private readonly IImageHelper _imageHelper;
+		public TeamService(IUnitOfWork unitOfWork, IMapper mapper, IImageHelper imageHelper)
 		{
 			_unitOfWork = unitOfWork;
 			_mapper = mapper;
 			_repository = _unitOfWork.GetGenericRepository<Team>();
+			_imageHelper = imageHelper;
 		}
 		public async Task<List<TeamListVM>> GetAllListAsync()
 		{
@@ -29,6 +33,14 @@ namespace PlumpingCareSystem.Service.ServiceHolding.WebApplication.Concrete
 		}
 		public async Task AddTeamAsync(TeamAddVM request)
 		{
+			var imageResult = await _imageHelper.ImageUpload(request.Photo, ImageType.team, null);
+			if (imageResult.Error != null)
+			{
+				return;
+			}
+			request.FileName = imageResult.Filename!;
+			request.FileType = imageResult.FileType!;
+
 			var team = _mapper.Map<Team>(request);
 			await _repository.AddEntityAsync(team);
 			await _unitOfWork.CommitAsync();
@@ -38,6 +50,8 @@ namespace PlumpingCareSystem.Service.ServiceHolding.WebApplication.Concrete
 			var team = await _repository.GetEntityByIdAsync(id);
 			_repository.DeletetEntity(team);
 			await _unitOfWork.CommitAsync();
+			_imageHelper.DeleteImage(team.FileName);
+
 		}
 		public async Task<TeamUpdateVM> GetTeamById(int id)
 		{
@@ -46,9 +60,26 @@ namespace PlumpingCareSystem.Service.ServiceHolding.WebApplication.Concrete
 		}
 		public async Task UpdateTeamAsync(TeamUpdateVM request)
 		{
+			var oldTeam = await _repository.Where(x => x.Id == request.Id).AsNoTracking().FirstAsync();
+			if (request.Photo != null)
+			{
+				var imageResult = await _imageHelper.ImageUpload(request.Photo, ImageType.team, null);
+				if (imageResult.Error != null)
+				{
+					return;
+				}
+				request.FileName = imageResult.Filename!;
+				request.FileType = imageResult.FileType!;
+			}
+
 			var team = _mapper.Map<Team>(request);
 			_repository.UpdatetEntity(team);
 			await _unitOfWork.CommitAsync();
+
+			if (request.Photo != null)
+			{
+				_imageHelper.DeleteImage(oldTeam.FileName);
+			}
 		}
 	}
 }
--- PlumpingCareSystem.Service/ServiceHolding/WebApplication/Concrete/TestimonalService.cs
+++ PlumpingCareSystem.Service/ServiceHolding/WebApplication/Concrete/TestimonalService.cs
@@ -3,10 +3,12 @@
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
+using PlumpingCareSystem.Core.Enumerators;
 using PlumpingCareSystem.Entity.WebApplication.Entities;
 using PlumpingCareSystem.Entity.WebApplication.ViewModels.Testimonal;
 using PlumpingCareSystem.Repository.Repositories.Abstract;
 using PlumpingCareSystem.Repository.UnitOfWorks.Abstract;
+using PlumpingCareSystem.Service.Helpers.Generic.Image;
 using PlumpingCareSystem.Service.ServiceHolding.WebApplication.Abstract;
 
 namespace PlumpingCareSystem.Service.ServiceHolding.WebApplication.Concrete
@@ -16,11 +18,13 @@ namespace PlumpingCareSystem.Service.ServiceHolding.WebApplication.Concrete
 		private readonly IUnitOfWork _unitOfWork;
 		private readonly IMapper _mapper;
 		private readonly IGenericRepositories<Testimonal> _repository;
-		public TestimonalService(IUnitOfWork unitOfWork, IMapper mapper)
+		private readonly IImageHelper _imageHelper;
+		public TestimonalService(IUnitOfWork unitOfWork, IMapper mapper, IImageHelper imageHelper)
 		{
 			_unitOfWork = unitOfWork;
 			_mapper = mapper;
 			_repository = _unitOfWork.GetGenericRepository<Testimonal>();
+			_imageHelper = imageHelper;
 		}
 		public async Task<List<TestimonalListVM>> GetAllListAsync()
 		{
@@ -29,6 +33,13 @@ namespace PlumpingCareSystem.Service.ServiceHolding.WebApplication.Concrete
 		}
 		public async Task AddTestimonalAsync(TestimonalAddVM request)
 		{
+			var imageResult = await _imageHelper.ImageUpload(request.Photo, ImageType.testimonal, null);
+			if (imageResult.Error != null)
+			{
+				return;
+			}
+			request.FileName = imageResult.Filename!;
+			request.FileType = imageResult.FileType!;
 			var testimonal = _mapper.Map<Testimonal>(request);
 			await _repository.AddEntityAsync(testimonal);
 			await _unitOfWork.CommitAsync();
@@ -38,6 +49,8 @@ namespace PlumpingCareSystem.Service.ServiceHolding.WebApplication.Concrete
 			var testimonal = await _repository.GetEntityByIdAsync(id);
 			_repository.DeletetEntity(testimonal);
 			await _unitOfWork.CommitAsync();
+			_imageHelper.DeleteImage(testimonal.FileName);
+
 		}
 		public async Task<TestimonalUpdateVM> GetTestimonalById(int id)
 		{
@@ -46,9 +59,24 @@ namespace PlumpingCareSystem.Service.ServiceHolding.WebApplication.Concrete
 		}
 		public async Task UpdateTestimonalAsync(TestimonalUpdateVM request)
 		{
+			var oldTestimonal = await _repository.Where(x => x.Id == request.Id).AsNoTracking().FirstAsync();
+			if (request.Photo != null)
+			{
+				var imageResult = await _imageHelper.ImageUpload(request.Photo, ImageType.testimonal, null);
+				if (imageResult.Error != null)
+				{
+					return;
+				}
+				request.FileName = imageResult.Filename!;
+				request.FileType = imageResult.FileType!;
+			}
 			var testimonal = _mapper.Map<Testimonal>(request);
 			_repository.UpdatetEntity(testimonal);
 			await _unitOfWork.CommitAsync();
+			if (request.Photo != null)
+			{
+				_imageHelper.DeleteImage(oldTestimonal.FileName);
+			}
 		}
 	}
 }
