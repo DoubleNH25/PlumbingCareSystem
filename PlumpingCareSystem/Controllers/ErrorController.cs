41bc4fa445901122ce81bc53fd117bd7d25dd31c
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-22 09:56:41 +0700

    Logging With NLog

--- PlumpingCareSystem/Controllers/ErrorController.cs
+++ PlumpingCareSystem/Controllers/ErrorController.cs
@@ -9,6 +9,14 @@ namespace PlumpingCareSystem.Controllers
 {
 	public class ErrorController : Controller
 	{
+		private readonly ILogger<ErrorController> _logger;
+
+		public ErrorController(ILogger<ErrorController> logger)
+		{
+			_logger = logger;
+		}
+
+
 		public IActionResult GeneralExceptions()
 		{
 			var exceptions = HttpContext.Features.Get<IExceptionHandlerFeature>()!.Error;
@@ -22,8 +30,9 @@ namespace PlumpingCareSystem.Controllers
 			if (exceptions.InnerException is SqlException sqlException && sqlException.Number == 547)
 				return View(new ErrorVM("You have to delete all relevant data before to move on.", 401));
 
+			_logger.LogError("The Error Message From System : -----" + exceptions.Message + "-----");
 
-			return View(new ErrorVM("Server error. Please speak your admin.", 500));
+			return View(new ErrorVM("Server error. Please speak to your admin.", 500));
 		}
 		public IActionResult PageNotFound()
 		{
--- PlumpingCareSystem/Program.cs
+++ PlumpingCareSystem/Program.cs
@@ -1,3 +1,4 @@
+using NLog.Web;
 using NToastNotify;
 using PlumpingCareSystem.Repository.Extensions;
 using PlumpingCareSystem.Service.Extensions;
@@ -15,17 +16,20 @@ builder.Services.LoadRepositoryLayerExtensions(builder.Configuration);
 builder.Services.LoadServiceLayerExtensions(builder.Configuration);
 
 
+// NLog: Setup NLog for Dependency injection
+builder.Logging.ClearProviders();
+builder.Host.UseNLog();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-	app.UseExceptionHandler("/Error/GeneralExceptions");
 
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
-
+app.UseExceptionHandler("/Error/GeneralExceptions");
 app.UseStatusCodePagesWithReExecute("/Error/PageNotFound");
 
 app.UseHttpsRedirection();
