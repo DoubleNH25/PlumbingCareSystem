30321af3c17e40e11200ee5bfd50dd25c7661a7e
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-23 08:53:06 +0700

    Completed For Category View

---
+++ PlumpingCareSystem/Components/CategoryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Service.ServiceHolding.WebApplication.Abstract;
+
+namespace PlumpingCareSystem.Components
+{
+	public class CategoryViewComponent : ViewComponent
+	{
+		private readonly ICategoryService _categoryService;
+
+		public CategoryViewComponent(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var categoryList = await _categoryService.GetAllListForUIAsync();
+			return View(categoryList);
+		}
+	}
+}
