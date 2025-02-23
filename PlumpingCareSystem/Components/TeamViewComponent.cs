6fadaf37bb31994674cfc7979a9fd236fe7c3c78
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-23 09:00:05 +0700

    Completed Team View

---
+++ PlumpingCareSystem/Components/TeamViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Service.ServiceHolding.WebApplication.Abstract;
+
+namespace PlumpingCareSystem.Components
+{
+	public class TeamViewComponent : ViewComponent
+	{
+		private readonly ITeamService _teamService;
+
+		public TeamViewComponent(ITeamService teamService)
+		{
+			_teamService = teamService;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var teamList = await _teamService.GetAllListForUIAsync();
+			return View(teamList);
+		}
+	}
+}
