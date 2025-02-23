4cde5dbdc4d4c0c47c3998e0af5933e7c267a17d
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-23 08:54:59 +0700

    Completed Contact View

---
+++ PlumpingCareSystem/Components/ContactViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Service.ServiceHolding.WebApplication.Abstract;
+
+namespace PlumpingCareSystem.Components
+{
+	public class ContactViewComponent : ViewComponent
+	{
+		private readonly IContactService _contactService;
+
+		public ContactViewComponent(IContactService contactService)
+		{
+			_contactService = contactService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var contactList = await _contactService.GetAllListForUIAsync();
+			return View(contactList);
+		}
+	}
+}
