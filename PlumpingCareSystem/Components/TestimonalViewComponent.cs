fa2379c096169cbccf4dd0d8c621e218f1c23248
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-23 09:01:51 +0700

    Completed Testimonal View

---
+++ PlumpingCareSystem/Components/TestimonalViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Service.ServiceHolding.WebApplication.Abstract;
+
+namespace PlumpingCareSystem.Components
+{
+	public class TestimonalViewComponent : ViewComponent
+	{
+		private readonly ITestimonalService _testimonalService;
+
+		public TestimonalViewComponent(ITestimonalService testimonalService)
+		{
+			_testimonalService = testimonalService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var testimonalList = await _testimonalService.GetAllListForUIAsync();
+			return View(testimonalList);
+		}
+	}
+}
