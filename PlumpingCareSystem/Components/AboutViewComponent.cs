267406d9d3fc12ed10151146abc222fe47ecfe0e
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-23 07:05:22 +0700

    About View

---
+++ PlumpingCareSystem/Components/AboutViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Service.ServiceHolding.WebApplication.Abstract;
+
+namespace PlumpingCareSystem.Components
+{
+	public class AboutViewComponent : ViewComponent
+	{
+		private readonly IAboutService _aboutService;
+
+		public AboutViewComponent(IAboutService aboutService)
+		{
+			_aboutService = aboutService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var aboutListForUI = await _aboutService.GetAllListForUIAsync();
+			return View(aboutListForUI);
+		}
+	}
+}
