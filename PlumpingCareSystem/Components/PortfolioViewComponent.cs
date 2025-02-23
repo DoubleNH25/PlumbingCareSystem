0c43b2029b4447ca7e27b6f96ce07745f7f6fc74
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-23 08:57:35 +0700

    Completed Portfolio View

---
+++ PlumpingCareSystem/Components/PortfolioViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Service.ServiceHolding.WebApplication.Abstract;
+
+namespace PlumpingCareSystem.Components
+{
+	public class PortfolioViewComponent : ViewComponent
+	{
+		private readonly IPortfolioService _portfolioService;
+
+		public PortfolioViewComponent(IPortfolioService portfolioService)
+		{
+			_portfolioService = portfolioService;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var portfolioList = await _portfolioService.GetAllListForUIAsync();
+			return View(portfolioList);
+		}
+	}
+}
