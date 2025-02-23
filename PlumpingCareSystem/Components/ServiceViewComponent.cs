8e0b4b4fe3045e567fe5d72243c88af82114c327
DoubleNH25 <145324178+DoubleNH25@users.noreply.github.com>
2025-02-23 08:58:54 +0700

    Completed Service View

---
+++ PlumpingCareSystem/Components/ServiceViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlumpingCareSystem.Service.ServiceHolding.WebApplication.Abstract;
+
+namespace PlumpingCareSystem.Components
+{
+	public class ServiceViewComponent : ViewComponent
+	{
+		private readonly IServiceService _serviceService;
+
+		public ServiceViewComponent(IServiceService serviceService)
+		{
+			_serviceService = serviceService;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var serviceListForUI = await _serviceService.GetAllListForUIAsync();
+			return View(serviceListForUI);
+		}
+	}
+}
